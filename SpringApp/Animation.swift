4d9cb148bd4dac5865e959817570bac506fe243f
AndyP <firreatwill@mail.ru>
2023-10-26 23:40:21 +0300

    fill models & services

--- SpringApp/Animation.swift
+++ SpringApp/Animation.swift
@@ -5,4 +5,33 @@
 //  Created by Андрей Платунов on 26.10.2023.
 //
 
-import Foundation
+struct Animation: CustomStringConvertible {
+    
+    let name: String
+    let curve: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+    
+    var description: String {
+        """
+preset: \(name)
+curve: \(curve)
+force: \(String(format: "%.02f", force))
+duration: \(String(format: "%.02f", duration))
+delay: \(String(format: "%.02f", delay))
+"""
+    }
+    
+    static func getRandomAnimation() -> Animation {
+        Animation(
+            name: DataStore.shared.animations.randomElement()?.rawValue ?? "slideLeft",
+            curve: DataStore.shared.animations.randomElement()?
+                .rawValue ?? "easeIn",
+            force: Double.random(in: 1...1.5),
+            duration: Double.random(in: 0.8...1.6),
+            delay: 2
+            
+        )
+    }
+}
--- SpringApp/DataStore.swift
+++ SpringApp/DataStore.swift
@@ -5,4 +5,45 @@
 //  Created by Андрей Платунов on 26.10.2023.
 //
 
-import Foundation
+import SpringAnimation
+
+final class DataStore {
+    
+    static let shared = DataStore()
+    
+    let animations: [AnimationPreset] = [
+        .fadeIn,
+        .fadeInDown,
+        .fadeInLeft,
+        .fadeInRight,
+        .fadeInUp,
+        .fadeOut,
+        .fadeOutIn,
+        .fall,
+        .flash,
+        .flipX,
+        .flipY,
+        .morph,
+        .pop,
+        .shake,
+        .slideDown,
+        .slideLeft,
+        .slideRight,
+        .slideUp,
+        .squeeze,
+        .squeezeDown,
+        .squeezeLeft,
+        .squeezeRight,
+        .squeezeUp,
+        .swing,
+        .zoomIn,
+        .wobble,
+        .zoomOut
+    ]
+    
+    let curves = AnimationCurve.allCases
+    
+    private init() {}
+}
+
+
