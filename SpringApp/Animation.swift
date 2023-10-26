7a3fda22f71ae1fc9aa069c323e3c1bc23e8c668
AndyP <firreatwill@mail.ru>
2023-10-26 22:31:00 +0300

    add services and models

---
+++ SpringApp/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  SpringApp
+//
+//  Created by Андрей Платунов on 26.10.2023.
+//
+
+import Foundation
---
+++ SpringApp/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  SpringApp
+//
+//  Created by Андрей Платунов on 26.10.2023.
+//
+
+import Foundation
